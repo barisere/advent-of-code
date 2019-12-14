1e716e2a809d0e964ed7dd73f0e7f7aec8aa0210
Barisere Jonathan <barry.jonathanks@gmail.com>
2019-12-14 23:51:05 +0100

    Fix unresolved identifier after refactor.

--- bin/crossed_wires.ml
+++ bin/crossed_wires.ml
@@ -15,7 +15,7 @@ let command = Command.basic ~summary:"Compute Manhattan distance with inputs fro
                let direction_codes1 = List.map line1 ~f:direction_code_of_string in
                let direction_codes2 = List.map line2 ~f:direction_code_of_string in
                let intersections = find_intersections (create_moves direction_codes1) (create_moves direction_codes2) in
-               let distances = List.map intersections ~f:(fun (x, y, _) -> manhattan_distance (x, y) Grid.origin)
+               let distances = List.map intersections ~f:(fun (x, y, _) -> manhattan_distance (x, y) coordinate_origin)
                                |> List.sort ~compare:Int.compare
                in
                let steps_to_first_intersection = match intersections with
